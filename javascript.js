c2cc7441f67bae3edb43b6f3b94771d2b7eeee6d
Kristian Tufte Sterud <kristian0805@hotmail.com>
2025-08-19 13:39:47 +0200

    Created a functioning etch-a-sketch with single colour and resize button

--- javascript.js
+++ javascript.js
@@ -1,8 +1,31 @@
 const content = document.querySelector(".container");
+const resizeGridBtn = document.querySelector("#resizeGrid");
+let gridSize = 16;
 
-for (let i = 0; i < 16 * 16; i++) {
-  const div = document.createElement
-  ("div");
-  div.className = "pixel"
-  content.appendChild(div);
+function createGrid() {
+  content.innerHTML = "";
+  for (let i = 0; i < gridSize * gridSize; i++) {
+    const pixel = document.createElement("div");
+    pixel.className = "pixel";
+    pixel.style.width = `${100 / gridSize}%`;
+    pixel.style.height = `${100 / gridSize}%`;
+    content.appendChild(pixel);
+  }
+}
+
+content.addEventListener("mouseover", (event) => {
+  let target = event.target;
+  if (target.classList.contains("pixel")) {
+    target.classList.add("colored");
+  }
+});
+
+resizeGridBtn.addEventListener("click", resizeGrid);
+
+function resizeGrid() {
+  let newSize = prompt("What size grid would you like? 0-150");
+  if (newSize > 0 && newSize <= 150) {
+    gridSize = newSize;
+  }
+  createGrid();
 }
